2b4d362c4a8972abd3dc99a1a58e7536d104c817
Tomoharu <n.tomo.cyclon@gmail.com>
2021-08-25 22:29:00 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+// global
+var str1 = 'abc';
+var TEST = '';
+
+// 無名関数（即時実行）
+(function() {
+  TEST = 'test';
+})();
+
+console.log('無名関数テスト：' + TEST);
+
+// switch文
+var animal = 'mimura';
+switch (animal) {
+  case 'cat':
+    console.log('にゃー');
+    break;
+  case 'dog':
+    console.log('わん');
+    break;
+  case 'bird':
+    console.log('ちゅん');
+    break;
+  default:
+    console.log('何もないのかよ！');
+}
+
+// デフォルト値設定
+var sayHello = function(str) {
+  var name = str || 'John Doe';
+  console.log('Hello, ' + name + '!');
+}
+sayHello('');
+
+// Array操作
+var array = ['nori', 'yukka', 'shishi'];
+//array.splice(0, 1);
+console.log(array);
+var newArray = array.slice(0, 1);
+console.log(newArray);
+
+// DOM確認
+window.onload = function() {
+  var main = document.getElementById('main');
+  var target = document.getElementById('target');
+
+  // eventListnerの確認
+  main.addEventListener('click', function(){
+    console.log(this);
+  });
+  target.addEventListener('click', function(e){
+    console.log(this);
+    e.stopPropagation();
+  });
+};
